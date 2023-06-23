7f07947501fc08fecda5df6722814c759fb9ee31
Rohanksingh18 <rohanksingh18@gmail.com>
2023-06-23 18:31:27 -0400

    changes requests

--- ecom_test/src/api_helpers/CouponsAPIHelper.py
+++ ecom_test/src/api_helpers/CouponsAPIHelper.py
@@ -4,26 +4,26 @@ import logging as logger
 
 
 class CouponsAPIHelper:
-    # class has an __init__ method that initializes an instance of the WooAPIUtility class.
+    # class has an '__init__' method that initializes an instance of the WooAPIUtility class.
     def __init__(self):
         self.woo_api_utility = WooAPIUtility()
 
-    # Method makes a POST request to create a new coupon by calling the post-method of the woo_api_utility instance.
+    # Method makes a POST request to create a 'new_coupon' by calling the post-method of the woo_api_utility instance.
     def call_create_coupon(self, payload):
         logger.debug("Calling 'Create Coupon'.")
         return self.woo_api_utility.post('coupons', params=payload, expected_status_code=201)
 
-    #  Calling the get method of the woo_api_utility instance with the coupon ID as a parameter.
+    # Calling 'retrieve_coupon'the get method of the woo_api_utility instance with the coupon ID as a parameter.
     def call_retrieve_coupon(self, coupon_id):
         logger.debug(f"Calling retrieve a coupon. Coupon id: {coupon_id}")
         return self.woo_api_utility.get(f'coupons/{coupon_id}')
 
-    # calling the get method of the woo_api_utility instance.
+    # Calling 'list all coupons'.the get method of the woo_api_utility instance.
     def call_list_all_coupons(self, payload=None):
         logger.debug("Calling list all coupons.")
         return self.woo_api_utility.get('coupons', params=payload)
 
-    # This method makes a DELETE request to delete a coupon.
+    # This method makes a 'DELETE' request to delete a coupon.
     def call_delete_coupon(self, coupon_id, force=True):
         logger.debug(f"Calling list all coupons.")
         return self.woo_api_utility.delete(f'coupons/{coupon_id}', params={"force": force})
--- ecom_test/src/pages/locators/HomePageLocators.py
+++ ecom_test/src/pages/locators/HomePageLocators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 class HomePageLocators:
-    Add_To_Cart_BTN = (By.XPATH,"//a[@aria-label='Add “Album” to your cart']")
-    Page_heading = (By.CSS_SELECTOR,".woocommerce-products-header__title.page-title")
-    Product_Elements = (By.CSS_SELECTOR, "ul.products li.product")  # all the product elements on home_page.
+    ADD_TO_CART_BTN = (By.XPATH, "//a[@aria-label='Add “Album” to your cart']")
+    PAGE_HEAD = (By.CSS_SELECTOR, ".woocommerce-products-header__title.page-title")
+    PROD_ELEM = (By.CSS_SELECTOR, "ul.products li.product")  # all the product elements on home_page.
 
